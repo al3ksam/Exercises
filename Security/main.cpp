49ac6aa1133627b61011741336cdda1a3a7de574
al3ksam <al3ksam@ya.ru>
2020-11-11 23:39:13 +0400

    Update

--- Security/main.cpp
+++ Security/main.cpp
@@ -1,8 +1,47 @@
 #include <iostream>
+#include <string>
+#include <iterator>
+#include <algorithm>
+#include <functional>
+
+namespace security
+{
+	enum class State
+	{
+		kDefault,
+		kAlarm,
+		kQuiet
+	};
+
+	void checkFloor(const std::string& floor, State& securityState)
+	{
+		securityState = State::kQuiet;
+	}
+}
 
 int main(int argc, char** argv)
 {
-	// TODO
+	std::function<void(const std::string&)> output([](const std::string& msg)
+	{
+		std::cout << msg << std::endl;
+	});
+
+	std::string sFloor;
+
+	std::getline(std::cin, sFloor);
+
+	security::State securityState = security::State::kDefault;
+
+	security::checkFloor(sFloor, securityState);
+
+	if (securityState == security::State::kAlarm)
+	{
+		output("ALARM");
+	}
+	else
+	{
+		output("quiet");
+	}
 
 	return 0;
 }
