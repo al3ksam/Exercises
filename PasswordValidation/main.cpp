456e1b55b8183570c70a130600be09792c56b0ff
al3ksam <al3ksam@ya.ru>
2020-11-11 13:28:00 +0400

    Update

--- PasswordValidation/main.cpp
+++ PasswordValidation/main.cpp
@@ -11,6 +11,18 @@ namespace pw
 
 int main(int argc, const char** argv)
 {
+	std::string password;
+
+	std::getline(std::cin, password);
+
+	if (pw::isValidPassword(password))
+	{
+		std::cout << "Strong" << std::endl;
+	}
+	else
+	{
+		std::cout << "Weak" << std::endl;
+	}
 
 	return 0;
 }
