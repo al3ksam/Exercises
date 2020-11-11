f7dfa64093e8a916b8107cd28ecae0c5aa090d0c
al3ksam <al3ksam@ya.ru>
2020-11-11 16:57:49 +0400

    Update

--- PasswordValidation/main.cpp
+++ PasswordValidation/main.cpp
@@ -1,11 +1,58 @@
 #include <iostream>
 #include <string>
+#include <algorithm>
+#include <iterator>
 
 namespace pw
 {
 	bool isValidPassword(std::string& password)
 	{
-		return false;
+		constexpr short kMinPwdLength{ 7 };
+
+		if (password.length() < kMinPwdLength) return false;
+
+		const std::string sSpecialCharacters { "!@#$%&*"	};
+		const std::string sNumbers			 { "0123456789" };
+
+		constexpr short kMinSpecialChars{ 2 };
+		constexpr short kMinNumbers{ 2 };
+
+		short nSpecialChars{ 0 };
+		short nNumbers{ 0 };
+
+		bool bIsValid = false;
+
+		for 
+		(
+			std::string::const_iterator pwdIt = password.cbegin(),
+										cIt   = std::string::const_iterator(); 
+			pwdIt != password.cend(); ++pwdIt
+		)
+		{
+			if (nSpecialChars < kMinSpecialChars)
+			{
+				cIt = std::find(sSpecialCharacters.cbegin(), sSpecialCharacters.cend(), *pwdIt);
+				if (cIt != sSpecialCharacters.cend())
+				{
+					++nSpecialChars;
+				}
+			}
+
+			if (nNumbers < kMinNumbers)
+			{
+				cIt = std::find(sNumbers.cbegin(), sNumbers.cend(), *pwdIt);
+				if (cIt != sNumbers.cend())
+				{
+					++nNumbers;
+				}
+			}
+
+			bIsValid = nSpecialChars == kMinSpecialChars && nNumbers == kMinNumbers;
+			
+			if (bIsValid) break;
+		}
+
+		return bIsValid;
 	}
 }
 
