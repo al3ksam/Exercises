4d3568fa88bbd67343921e6b5ff66a069a50045f
al3ksam <al3ksam@ya.ru>
2020-11-18 15:21:01 +0400

    Update

--- Security/main.cpp
+++ Security/main.cpp
@@ -19,24 +19,27 @@ namespace security
 		constexpr char chGuard{ 'G' };
 		constexpr char chThief{ 'T' };
 
-		bool bHaveGuard = false;
-		bool bHaveThief = false;
+		bool bMoney = false;
+		bool bGuard = false;
+		bool bThief = false;
+
+		bool bAlarm = false;
 
 		// xxxxxGxx$xxxT
 		for (std::string::const_iterator cIt{ floor.cbegin() }; cIt != floor.cend(); ++cIt)
 		{
-			if (*cIt == chGuard && bHaveGuard == false)
+			if (*cIt == chGuard)
 			{
-				bHaveGuard = true;
+				bGuard = true;
 			}
 			
-			if (*cIt == chMoney && bHaveGuard == true)
+			if (*cIt == chThief)
 			{
-				securityState = State::kQuiet;
+				
 			}
 		}
 		 
-		//securityState = State::kQuiet;
+		securityState = bAlarm == true ? State::kAlarm : State::kQuiet;
 	}
 }
 
